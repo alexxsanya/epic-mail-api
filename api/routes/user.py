ba46343edd2da9068b70bcb57090668408dce964
alexxsanya <alexxsanya@gmail.com>
2019-03-23 10:34:39 +0300

    Added api to login a user [#164792653]

--- api/routes/user.py
+++ api/routes/user.py
@@ -14,3 +14,17 @@ def create_user():
             password = user.get('password'),
             recovery_email = user.get('recovery_email')
         ).create_user()
+
+@user_api.route('/auth/login', methods=['POST'])
+def login():
+    user = request.get_json()
+    is_logged = User.login_user(
+        email = user.get('email'),
+        password = user.get('password')
+    )
+
+    if not is_logged:
+        return jsonify({
+            'error':'No user with supplied password or email address',
+            'status':400,
+        })
