7feb66301bff51dee4dd250bede390a9de10dcd3
alexxsanya <alexxsanya@gmail.com>
2019-03-23 17:36:58 +0300

    Added message route to /route [#164846349]

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
 from .user import user_api
+from .message import msg_api
 app = Flask(__name__)
 app.register_blueprint(user_api, url_prefix="/api/v1")
+app.register_blueprint(msg_api,url_prefix="/api/v1")
