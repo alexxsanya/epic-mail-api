fa28ef4d548d71e98eef77bc72023d4fc6376ea9
alexxsanya <alexxsanya@gmail.com>
2019-03-23 17:36:02 +0300

    Added a api for sending a message [#164846349]

---
+++ api/routes/message.py
@@ -0,0 +1,22 @@
+from flask import Blueprint,jsonify, request, Response
+from api.models import Message
+from api.util import MessageValidator
+msg_api = Blueprint("msg_api", __name__)
+
+@msg_api.route('/messages',methods=['POST'])
+def send_message():
+    msg = request.get_json()
+    if msg == None: 
+        return jsonify({
+            'error':'Invalid Request Body',
+            'status': 400
+        })
+
+    if MessageValidator().validator(msg):
+        Message(
+            subject = msg.get('subject',None),
+            msgBody= msg.get('msgBody',None),
+			parentId = msg.get('parentId',None),
+            sender = msg.get('sender',None),
+            reciever = msg.get('reciever',None)
+        ).send_message()
