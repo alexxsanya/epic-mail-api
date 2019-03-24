bfa91bd261d23b68328d8ecdec55a6c91ee564ac
alexxsanya <alexxsanya@gmail.com>
2019-03-24 18:54:57 +0300

    Refactored user model code [#164792881]

--- api/models/user.py
+++ api/models/user.py
@@ -1,6 +1,11 @@
 import datetime
-from flask import abort, make_response, jsonify
-from api.util import Auth,UserValidator
+from flask import abort, make_response, jsonify,g
+from api.util import UserValidator
+from flask_jwt_extended import (
+    JWTManager, jwt_required, 
+    create_access_token, 
+    get_jwt_claims, decode_token)
+    
 class User():
 
     user_db = [{
@@ -8,13 +13,15 @@ class User():
         'email':'alex@epicmail.com',
         'lastname':'alex',
         'firstname':'James',
-        'recovery_email':'james@gmail.com'
+        'recovery_email':'james@gmail.com',
+        'password':'Alex@11'
         },{
         'id':2,
         'email':'steven@epicmail.com',
         'lastname':'steven',
         'firstname':'walton',
-        'recovery_email':'walton@gmail.com'            
+        'recovery_email':'walton@gmail.com',
+        'password':'Walton@2'            
         }]
 
     def __init__(self,email,firstname,lastname,password,recovery_email):
@@ -36,13 +43,12 @@ class User():
                 'password':self.password,
                 'recovery_email':self.recovery_email
             })            
-            token = Auth().encode_token(self.email)
-            
+            token = create_access_token(identity=self.id)
             return abort(
                 jsonify({
                 'status':201,
                 'data':[{
-                        'token':token.decode("utf-8"),
+                        'token':token,
                         'user':{
                             'id':self.id,
                             'firstname':self.firstname
@@ -71,12 +77,12 @@ class User():
             user = [u for u in User.user_db if ((u['email'] == email) \
                     and (u['password'] == password))]
             if len(user) == 1:
-                token = Auth().encode_token(email)
+                token = create_access_token(identity=user[0]['id'])
                 abort(
                     jsonify({
                     'status':200,
                     'data':[{
-                            'token':token.decode("utf-8"),
+                            'token':token,
                             'user':{
                                 'id':user[0]['id'],
                                 'firstname':user[0]['firstname']
