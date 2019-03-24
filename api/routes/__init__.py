8b0f1cb5063550ce9447f6c17883dd9b550b8e7a
alexxsanya <alexxsanya@gmail.com>
2019-03-24 14:40:57 +0300

    Initialized jwt [#164792881]

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from .user import user_api
 from .message import msg_api
+from flask_jwt_extended import (
+    JWTManager, jwt_required, 
+    create_access_token, 
+    get_jwt_claims, decode_token)
+
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'my_secrete_key'
+jwt = JWTManager(app)
 app.register_blueprint(user_api, url_prefix="/api/v1")
 app.register_blueprint(msg_api,url_prefix="/api/v1")
