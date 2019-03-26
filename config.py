124a873c83edef83ab59532e2604d572ee87dd27
alexxsanya <alexxsanya@gmail.com>
2019-03-27 00:33:30 +0300

    Add the database url constants [#164908061]

--- config.py
+++ config.py
@@ -3,22 +3,26 @@ from os import environ
 class Config(object):
     DEBUG = False
     TESTING = False
+    DATABASE_URI = environ.get('DATABASE_URL')
+    JWT_SECRET_KEY = environ.get('SECRET_KEY') 
 
 class ProductionConfig(Config):
-    ENV = "production"
     DEBUG = False
 
-class StagingConfig(Config):
-    ENV = "staging"
-    DEVELOPMENT = True
+class StagingConfig(Config): 
     DEBUG = True
 
 class DevelopmentConfig(Config):
-    ENV = "development"
-    DEVELOPMENT = True
     DEBUG = True
 
 class TestingConfig(Config):
     TESTING = True
-    ENV = "testing"
     DEBUG = True
+    DATABASE_URL = environ.get("DATABASE_URL_TEST")
+
+app_config = {
+    "development" : DevelopmentConfig,
+    "testing" : TestingConfig,
+    "production" : ProductionConfig,
+    "staging": StagingConfig
+}
