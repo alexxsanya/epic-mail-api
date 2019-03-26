fa6a0709a9869799c89b413cd0d2db084ddeb070
alexxsanya <alexxsanya@gmail.com>
2019-03-27 00:24:10 +0300

    Add the db_manager [#164908061]

---
+++ api/util/db_manager.py
@@ -0,0 +1,47 @@
+import psycopg2
+from config import app_config
+
+class DB_Manager:
+
+    def __init__(self,app_env):
+        self.conn = None
+        self.app_env = app_env
+
+    def run_query(self,query,query_option="default"):
+        try:
+            self.conn = psycopg2.connect(app_config[self.app_env].DATABASE_URL)
+            cur = self.conn.cursor()
+            cur.execute(query)
+            
+            option = {
+                'default':cur,
+                'fetch_all': cur.fetchall(),
+                'fetch_one': cur.fetchone(),
+            }
+
+            cur.close
+
+            return option.get(query_option, "Invalid Option")
+
+        except psycopg2.DatabaseError as error:
+            return error
+
+    def create_table(self):
+
+        sql_file = open('api/tables.sql','r') 
+        
+        self.run_query(query=sql_file.read())
+
+        self.conn.commit()
+        self.conn.close()
+
+    def drop_tables(self):
+        query = """
+                DROP TABLES users, messages,
+                            messages_sent, messages_received,
+                            groups, group_users;
+                """ 
+        self.run_query(query)
+        self.conn.commit()
+        self.conn.close()
+
