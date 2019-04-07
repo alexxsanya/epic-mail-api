27046526d358963a91bc07ce0bcd8159b6b01578
alexxsanya <alexxsanya@gmail.com>
2019-04-07 18:03:29 +0300

    add routes for the ui templates [#164979422]

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from .user import user_api
 from .message import msg_api
 from .group import g_api
@@ -17,6 +17,27 @@ from flask_cors import CORS
 def create_app(app_env):
 
     app = Flask(__name__, instance_relative_config=True)
+
+    @app.route('/', methods=['GET'])
+    def home():
+        return render_template("index.html")
+
+    @app.route('/login.html', methods=['GET'])
+    def login():
+        return render_template("login.html")
+
+    @app.route('/signup.html', methods=['GET'])
+    def signup():
+        return render_template("signup.html")
+
+    @app.route('/components/groups.html', methods=['GET'])
+    def group():
+        return render_template("./components/groups.html")
+
+    @app.route('/components/compose.html', methods=['GET'])
+    def compose():
+        return render_template("./components/compose.html")
+
     app.config.from_object(app_config[str(app_env)])
     JWTManager(app)
     DB_Manager(app_env).create_tables()
